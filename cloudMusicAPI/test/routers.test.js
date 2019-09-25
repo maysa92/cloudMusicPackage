2a54dd4161e4449d67400e65e753cc9246245b2b
SinRou Chen <appleswill29@gmail.com>
2019-09-24 21:22:58 -0400

    Update routers.test.js

--- cloudMusicAPI/test/routers.test.js
+++ cloudMusicAPI/test/routers.test.js
@@ -1,23 +1,25 @@
+
+// test searching
 const crypto = require('crypto')
 const assert = require('assert')
 const { createRequest } = require('../utils/util')
 
-describe('test searching function', () => {
-  it('the name captured should be identical to the one entered', done => {
+describe('test searching function', function() {
+  it('check if the name captured is identical to the one entered', function() {
     const s = '大海'
     const stype = 1
-    const limit = 30
     const data =
-      's=' + s + '&limit=' + limit + '&type=' + stype + '&offset=0'
+      's=' + s  + '&type=' + stype + '&offset=0'
     
-      createRequest('/api/search/pc/', 'POST', data)
-      .then(result => {
-        console.log(JSON.parse(result).result.songs[0].mp3Url)
-        assert(JSON.parse(result).result.songs[0].name === '大海')
-        done()
-      })
-      .catch(err => {
-        done(err)
-      })
+  createRequest('/api/search/pc/', 'POST', data)
+  try {
+  console.log(JSON.parse(result).result.songs[0].mp3Url)
+  assert(JSON.parse(result).result.songs[0].name === '大海')
+  done()
+  })
+  catch(err) { done(err) })
   })
 })
+
+//test lyric fetching
+//test music url fetching
