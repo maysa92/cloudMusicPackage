5b2fdc7baa101bb0ce07aef30df928519a1f2044
SinRou Chen <appleswill29@gmail.com>
2019-09-24 20:34:50 -0400

    try first test

--- cloundMusicAPI/test/routers.test.js
+++ cloundMusicAPI/test/routers.test.js
@@ -1,19 +1,19 @@
-const assert = require('assert')
 const crypto = require('crypto')
+const assert = require('assert')
 const { createRequest } = require('../utils/util')
 
 describe('test searching function', () => {
   it('the name captured should be identical to the one entered', done => {
-    const s = '聽海'
+    const s = '大海'
     const stype = 1
     const limit = 30
-
     const data =
       's=' + s + '&limit=' + limit + '&type=' + stype + '&offset=0'
-    createRequest('/api/search/pc/', 'POST', data)
+    
+      createRequest('/api/search/pc/', 'POST', data)
       .then(result => {
         console.log(JSON.parse(result).result.songs[0].mp3Url)
-        assert(JSON.parse(result).result.songs[0].name === '聽海')
+        assert(JSON.parse(result).result.songs[0].name === '大海')
         done()
       })
       .catch(err => {
