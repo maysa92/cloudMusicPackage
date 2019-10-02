30bd6f3db6cb5a3716df3d5bf67a80375fe565f1
maysa92 <appleswill29@gmail.com>
2019-10-02 14:58:40 -0400

    edit test.js

--- cloudMusicAPI/test/routers.test.js
+++ cloudMusicAPI/test/routers.test.js
@@ -4,12 +4,11 @@ var assert = require('assert')
 var {createRequest} = require('../utils/util')
 
 describe('the searching function', function () {
+  var s = '可不可以', stype = 1
+  var data = 's=' + s + '&type=' + stype 
   // test if it connects successfully
   it('Response Status Codes should be 200', function (done) {
-    var s = '可不可以', stype = 1
-    var data = 's=' + s + '&type=' + stype 
-    
-  createRequest('/api/search/pc/', 'POST', data)
+    createRequest('/api/search/pc/', 'POST', data)
     .then(result => {
       var rscode = JSON.parse(result).code
         console.log('code:' + rscode)
@@ -20,10 +19,7 @@ describe('the searching function', function () {
   })
   // test if it catches info correctly
   it('the name captured should be identical to the one entered', function (done) {
-    var s = '可不可以', stype = 1
-    var data = 's=' + s + '&type=' + stype 
-    
-  createRequest('/api/search/pc/', 'POST', data)
+    createRequest('/api/search/pc/', 'POST', data)
     .then(result => {
       console.info(JSON.parse(result).result.songs[0].mp3Url)
       assert(JSON.parse(result).result.songs[0].name === '可不可以')
@@ -36,9 +32,9 @@ describe('the searching function', function () {
 
 // test the lyric fetching function
 describe('the lyric fetching function', function () {
+  var music_id = 268547
   // test if it fetches info
   it('should not be empty', function (done) {
-    var music_id = 268547
     createRequest(
       '/api/song/lyric?os=osx&id=' + music_id + '&lv=-1&kv=-1&tv=-1', 'GET', null
     )
@@ -52,7 +48,6 @@ describe('the lyric fetching function', function () {
   })
   // test if it fetches the right info
   it('should have lrc', function (done) {
-    var music_id = 268547
     createRequest(
       '/api/song/lyric?os=osx&id=' + music_id + '&lv=-1&kv=-1&tv=-1','GET', null
     )
